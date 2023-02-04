d313168e48bb725085e4d8af1ee4e4698beba31c
samdav3 <sdavenport@students.mchenry.edu>
2023-02-03 20:54:49 -0600

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+    var coffee_shop = arrayOf("Cortado", "Espresso", "Flat White", "Americano", "Drip Coffee")
+    for (arrayPart in coffee_shop){
+        println("$arrayPart")
+    }
+}
